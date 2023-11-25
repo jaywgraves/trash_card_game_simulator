9ba55b769c40aed81e2ee3b584da7fe970bbbaf6
jay graves <jaywgraves@gmail.com>
2023-11-25 16:35:46 -0600

    turn stats csv files into a sqlite db

---
+++ ddl.sql
@@ -0,0 +1,12 @@
+create table games (
+    game_nbr int,
+    rnd int,
+    rnd_type text,
+    p1_score int,
+    p2_score int,
+    margin int,
+    p1_streak int,
+    p2_streak int,
+    game_seed int,
+    rnd_seed int
+)
