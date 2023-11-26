6b0a96c9558d103218bd0c7dc20b6259da0789b1
jay graves <jaywgraves@gmail.com>
2023-11-26 10:08:54 -0600

    add another interesting game seed to replay

--- replay.py
+++ replay.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
 
     #seed, rnd_max = 347707, None     # playable ace exposes an ace on last round
     #seed, rnd_max = 676078, 1       # full perfect turn
-    seed, rnd_max = 504985, 17      # perfect turn on non-Final round
+    #seed, rnd_max = 504985, 17      # perfect turn on non-Final round
+    seed, rnd_max = 352596, 10       # 'headshot' perfect turn of 10 on round 10 FTW
+
     p1 = trashgame.Player('a')
     p2 = trashgame.Player('b')
     g = trashgame.Game(p1, p2, seed, show_output=True)
