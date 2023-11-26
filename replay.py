b90b21511cd56a17939b761e1fe005d5691bd090
jay graves <jaywgraves@gmail.com>
2023-11-25 19:11:00 -0600

    account for some weirdness in seed for replay script

--- replay.py
+++ replay.py
@@ -2,6 +2,7 @@ import trashgame
 
 if __name__ == "__main__":
     seed = 676079
+    seed -= 1   # because 1 gets added to rnd_cnt before even starting
     rnd_max = None
     rnd_max = 1
     p1 = trashgame.Player('a')
