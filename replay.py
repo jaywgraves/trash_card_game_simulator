9c045d874752fd143517295bae56f7860ed704c9
jay graves <jaywgraves@gmail.com>
2023-11-26 07:51:19 -0600

    start keeping a record of weird replay test cases

--- replay.py
+++ replay.py
@@ -1,11 +1,12 @@
 import trashgame
 
 if __name__ == "__main__":
-    seed = 676079
+    seed, rnd_max = 347707, None    # playable ace exposes an ace on last round
+    seed, rnd_max = 676079, 2       # full perfect turn
     seed -= 1   # because 1 gets added to rnd_cnt before even starting
-    rnd_max = None
-    rnd_max = 1
     p1 = trashgame.Player('a')
     p2 = trashgame.Player('b')
     g = trashgame.Game(p1, p2, seed, show_output=True)
     stats = g.play(0, rnd_max)
+    for s in stats:
+        print(s)
